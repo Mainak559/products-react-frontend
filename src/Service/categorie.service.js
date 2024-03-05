5e7fa84d2b9bf44f83336163a2fb16ca8df7d663
Mainak Halder <mainakhalder5598633@gmail.com>
2024-03-05 23:01:21 +0530

    update service url

--- src/Service/categorie.service.js
+++ src/Service/categorie.service.js
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8084';
+const API_URL = 'https://ems-backend-mainak.azurewebsites.net';
 
 class CategorieService {
   getAllCategories() {
--- src/Service/product.service.js
+++ src/Service/product.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8084"; 
+const API_URL = "https://ems-backend-mainak.azurewebsites.net"; 
 
 class ProductService {
 
